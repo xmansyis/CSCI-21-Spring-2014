bf487cec6494c534abad3b2e212829d851c7fb34
OpenShift guest <syis@live.com>
2014-04-07 12:40:03 -0400

    Programming Challenge 17

---
+++ Challenges/pc17/SLNode.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class SLNode{
+    private:
+        SLNode* next;
+        int content;
+        
+    public:
+        SLNode();
+        SLNode(int contents);
+        ~SLNode();
+        void setContents (int newContent);
+        int getContents () const;
+        void setNextNode(SLNode* next);
+        SLNode* getNextNode () const;
+        
+};
