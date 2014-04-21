cbd941f9211f8b92156525bb2bb42fc517a655f4
OpenShift guest <syis@live.com>
2014-04-21 12:38:36 -0400

    Programming Challenge 21

---
+++ Challenges/pc21/box.h
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+class Box{
+    
+    public:
+        Box(T newContent)
+            :contents(newContent){}
+        
+        void setContents(T newContent){
+            contents = newContent;            
+        }
+    
+        T getContents() const{
+            return contents;
+        }
+
+        friend ostream& operator << (ostream& outs, Box<T> e){
+            outs << e.contents;    
+        }
+  
+  private:
+    T contents;
+};
